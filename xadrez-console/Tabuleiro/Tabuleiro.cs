3a710479973dd6435bf8836965233d740279889b
rjsilvaoficial <rjsilvaoficial@gmail.com>
2022-02-01 07:07:53 -0300

    Tratando erros com exceção personalizada!

--- xadrez-console/Tabuleiro/Tabuleiro.cs
+++ xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using tabuleiro;
 
 namespace tabuleiro
 {
@@ -16,12 +17,25 @@ namespace tabuleiro
         {
             Linhas = linhas;
             Colunas = colunas;
-            pecas = new Peca[linhas, colunas];
+            pecas = new Peca[Linhas, Colunas];
         }
 
+        
         public Peca peca (int linha, int coluna)
         {
             return pecas[linha, coluna];
+        }
+        
+
+        public Peca peca (Posicao pos)
+        {
+            return pecas[pos.Linha, pos.Coluna];
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
         }
 
         public void colocarPeca(Peca p, Posicao pos) //atribua peça e posição destas à: peças[] e p.posição
@@ -29,6 +43,25 @@ namespace tabuleiro
             pecas[pos.Linha, pos.Coluna] = p;
             p.posicao = pos;
         }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if(pos.Linha < 0 || pos.Linha > Linhas || pos.Coluna < 0 || pos.Coluna > Colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
+        }
+
+
 
 
     }
---
+++ xadrez-console/Tabuleiro/TabuleiroException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    public class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base(msg) { }
+    }
+}
